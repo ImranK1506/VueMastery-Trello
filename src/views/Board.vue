401721e4421b536e56e55d9b5b6508e4bdd05a70
Imran Khan <imran.khan@kpn.com>
2020-05-08 13:12:48 +0200

    board.vue styling, loading data and localStorage

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,31 +1,54 @@
 <template>
   <div class="board">
+    <div class="flex flex-row items-start">
+      <div class="column"
+           v-for="(column, $columnIndex) of board.columns"
+           :key="$columnIndex">
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div class="task"
+               v-for="(task, $taskIndex) of column.tasks"
+              :key="$taskIndex">
+            <span class="w-full flex-no-shrink font-bold">
+            {{ task.name }}
+            </span>
+            <p class="w-full flex-no-shrink mt-1 text-sm" v-if="task.description">
+              {{ task.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script>
-export default {
+  import {mapState} from 'vuex';
 
-}
+  export default {
+    computed: mapState(['board']),
+  }
 </script>
 
 <style lang="css">
-.task {
-  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
-}
+  .task {
+    @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+  }
 
-.column {
-  @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
-  min-width: 350px;
-}
+  .column {
+    @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
+    min-width: 350px;
+  }
 
-.board {
-  @apply p-4 bg-teal-dark h-full overflow-auto;
-}
+  .board {
+    @apply p-4 bg-teal-dark h-full overflow-auto;
+  }
 
-.task-bg {
-  @apply pin absolute;
-  background: rgba(0,0,0,0.5);
-}
+  .task-bg {
+    @apply pin absolute;
+    background: rgba(0, 0, 0, 0.5);
+  }
 </style>
